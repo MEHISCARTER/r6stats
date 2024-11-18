f5eacb3598a72b6a9fdd72e4bbb35fb682b1b103
MEHISCARTER <carterpylman@gmail.com>
2024-11-18 10:11:45 -0500

    Updated database.py

--- src/database.py
+++ src/database.py
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS Match (
 match_id INTEGER PRIMARY KEY,
 date TEXT NOT NULL,
 map TEXT NOT NULL,
-result TEXT NOT NULL
+result TEXT
 )
 ''')
 
@@ -45,7 +45,7 @@ FOREIGN KEY (match_id) REFERENCES Match (match_id)
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS UserStats (
 stat_id INTEGER PRIMARY KEY AUTOINCREMENT,
-round_id INTEGER NOT NULL,
+round_id TEXT NOT NULL,
 operator TEXT NOT NULL,
 kills INTEGER DEFAULT 0,
 deaths INTEGER DEFAULT 0,
@@ -75,12 +75,14 @@ def new_match(date, map, result):
    return cursor.lastrowid #returns new match_id
 
 def new_round(match_id, round_number, result):
+   round_id = f"{match_id}_{round_number}"
+   
    cursor.execute('''
-   INSERT INTO Round (match_id, round_number, result)
-   VALUES (?, ?, ?)
-   ''', (match_id, round_number, result))
+   INSERT INTO Round (round_id, match_id, round_number, result)
+   VALUES (?, ?, ?, ?)
+   ''', (round_id, match_id, round_number, result))
    conn.commit()
-   return cursor.lastrowid #returns new round_id
+   return round_id #returns new round_id
 
 def new_user_stats(round_id, operator, kills, deaths, assists, site, side, result_type):
    cursor.execute('''
@@ -89,13 +91,32 @@ def new_user_stats(round_id, operator, kills, deaths, assists, site, side, resul
    ''', (round_id, operator, kills, deaths, assists, site, side, result_type))
    conn.commit()
 
+def change_result(match_id, res):
+   cursor.execute('''
+   UPDATE Match
+   SET result = ?
+   WHERE match_id = ?
+   ''', (res, match_id))
+   conn.commit()
+
 """
 Querying data for use in visualization and data analysis
 get_match_stats returns match stats based on match_id
 """
+def match_table():
+   cursor.execute("SELECT * FROM Match")
+   rows = cursor.fetchall()
+   return rows
 
-cursor.execute('SELECT * FROM Match')
-matches=cursor.fetchall()
+def round_table():
+   cursor.execute("SELECT * FROM Round")
+   rows = cursor.fetchall()
+   return rows
+
+def user_stats_table(): 
+   cursor.execute("SELECT * FROM UserStats")
+   rows = cursor.fetchall()
+   return rows
 
 def get_match_stats(match_id):
    cursor.execute('''
@@ -111,3 +132,13 @@ def check_match(match_id):
    cursor.execute("SELECT * FROM Match WHERE match_id = ?", (match_id,))
    match = cursor.fetchone()
    print("Match Found:", match)
+
+#testing
+
+def reset_tables():
+   cursor.execute("DROP TABLE IF EXISTS Match")
+   cursor.execute("DROP TABLE IF EXISTS Round")
+   cursor.execute("DROP TABLE IF EXISTS UserStats")
+   conn.commit()
+   conn.close()
+
